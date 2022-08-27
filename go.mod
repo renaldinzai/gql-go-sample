f190700bfb371e2e860eec139aa909bb817e777a
renaldin <renaldinzai@inovasi360.com>
2022-08-27 23:26:01 +0700

    Initial commits, add queries and mutations, integration to mysql database

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/renaldinzai/gql-go-sample
+go 1.19
+
+require (
+	github.com/99designs/gqlgen v0.17.15
+	github.com/vektah/gqlparser/v2 v2.4.8
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/mitchellh/mapstructure v1.3.1 // indirect
+)
---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/renaldinzai/gql-go-sample/graph"
+	"github.com/renaldinzai/gql-go-sample/graph/generated"
+	database "github.com/renaldinzai/gql-go-sample/internal/pkg/db/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	defer database.CloseDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
